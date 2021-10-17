133ff4b91384941833283c33744a2dfe2ace8a5c
elmi-elmi <shahrokhelmi@gmail.com>
2021-10-17 17:26:14 +0330

    add navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <nuxt/>
+    <TheHeader @sidenavToggle="displaySideNav = !displaySideNav" />
+    <TheSideNav :show="displaySideNav" @close="displaySideNav = false" />
+    <nuxt />
   </div>
 </template>
 
+<script>
+import TheHeader from "../components/Navigation/TheHeader.vue";
+import TheSideNav from "../components/Navigation/TheSidenav.vue";
+export default {
+  components: { TheHeader, TheSideNav },
+  data() {
+    return {
+      displaySideNav: false,
+    };
+  },
+};
+</script>
 <style>
 html {
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
 }
 
 body {
