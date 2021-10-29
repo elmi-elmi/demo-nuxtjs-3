425ceb5b88d39cb8df9909444a4751f14ea2fc12
elmi-elmi <shahrokhelmi@gmail.com>
2021-10-29 18:49:26 +0330

    remove async

--- pages/index.vue
+++ pages/index.vue
@@ -12,29 +12,11 @@ import PostList from "~/components/Posts/PostList";
 export default {
   name:'Pages',
   components: { PostList },
-  asyncData(context, callback){
-    console.log('-----> asyncData execute.')
-    setTimeout(()=>{
-      callback(null,{
-        loadedPosts:[
-        {
-          id:'1',
-          title:'First Post',
-          previewText:'This is our first post',
-          thumbnail:'"https://news.cgtn.com/news/2020-11-02/Analysis-China-is-betting-on-science-and-tech-like-never-before-V68V871ula/img/871ca9ce8b9941088260b6ed4ced4eeb/871ca9ce8b9941088260b6ed4ced4eeb.jpeg"'
-        },
-        {
-          id:'2',
-          title:'Second Post',
-          previewText:'This is our second post',
-          thumbnail:'"https://news.cgtn.com/news/2020-11-02/Analysis-China-is-betting-on-science-and-tech-like-never-before-V68V871ula/img/871ca9ce8b9941088260b6ed4ced4eeb/871ca9ce8b9941088260b6ed4ced4eeb.jpeg"'
-        }
-      ]
-
-      });
-
-    },1000)
-  },
+  computed:{
+    loadedPosts(){
+      return this.$store.getters.loadedPosts
+    }
+  }
   // data(){
   //   return{
   //     loadedPosts:
