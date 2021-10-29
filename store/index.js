170fe93a89ad7917af1bd32cddd6a726c723dfb9
elmi-elmi <shahrokhelmi@gmail.com>
2021-10-29 18:48:55 +0330

    nuxtServerInit

--- store/index.js
+++ store/index.js
@@ -1,22 +1,44 @@
 import Vuex from 'vuex';
 
-const createStore = ()=>{
+const createStore = () => {
     return new Vuex.Store({
-        state:{
-            loadedPosts:[]
+        state: {
+            loadedPosts: []
         },
-        mutations:{
-            setPosts(state, posts){
+        mutations: {
+            setPosts(state, posts) {
+                console.log('========>', posts)
                 state.loadedPosts = posts
             }
         },
-        actions:{
-            setPosts(contex){
-                contex.commit('setposts')
+        actions: {
+            nuxtServerInit(vuexContext,context) {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        vuexContext.commit('setPosts', [
+                            {
+                                id: '1',
+                                title: 'First Post',
+                                previewText: 'This is our first post',
+                                thumbnail: '"https://news.cgtn.com/news/2020-11-02/Analysis-China-is-betting-on-science-and-tech-like-never-before-V68V871ula/img/871ca9ce8b9941088260b6ed4ced4eeb/871ca9ce8b9941088260b6ed4ced4eeb.jpeg"'
+                            },
+                            {
+                                id: '2',
+                                title: 'Second Post',
+                                previewText: 'This is our second post',
+                                thumbnail: '"https://news.cgtn.com/news/2020-11-02/Analysis-China-is-betting-on-science-and-tech-like-never-before-V68V871ula/img/871ca9ce8b9941088260b6ed4ced4eeb/871ca9ce8b9941088260b6ed4ced4eeb.jpeg"'
+                            }
+                        ])
+                        resolve()
+                    }, 1000);
+                })
+            },
+            setPosts(vuexContex, posts) {
+                vuexContex.commit('setposts', posts)
             }
         },
-        getters:{
-            loadedPosts(state){
+        getters: {
+            loadedPosts(state) {
                 return state.loadedPosts
             }
         }
